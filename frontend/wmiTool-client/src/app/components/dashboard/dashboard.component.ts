3d88fc4be654ebd102b8ab135889237129b8fed6
gazawayj <gazaway85@gmail.com>
2026-03-13 23:34:59 -0600

    update component to remove rxjs

--- frontend/wmiTool-client/src/app/components/dashboard/dashboard.component.ts
+++ frontend/wmiTool-client/src/app/components/dashboard/dashboard.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit, OnDestroy, inject, Inject, PLATFORM_ID } from '@angular/core';
+import { Component, OnInit, inject, Inject, PLATFORM_ID } from '@angular/core';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { SystemService } from '../../services/wmi.service';
-import { interval, Subscription, forkJoin } from 'rxjs';
-import { startWith, switchMap } from 'rxjs/operators';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,7 +10,7 @@ import { startWith, switchMap } from 'rxjs/operators';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit, OnDestroy {
+export class DashboardComponent implements OnInit {
   cpu: any;
   mem: any;
   disk: any;
@@ -19,36 +18,26 @@ export class DashboardComponent implements OnInit, OnDestroy {
   lastUpdate!: Date;
 
   private sysServ = inject(SystemService);
-  private refreshSub!: Subscription;
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
 
   ngOnInit() {
-    // Only run polling in the browser (SSR-safe)
-    //if (isPlatformBrowser(this.platformId)) {
-      this.refreshSub = interval(2000)
-        .pipe(
-          startWith(0), // fetch immediately
-          switchMap(() =>
-            forkJoin({
-              cpu: this.sysServ.getCPU(),
-              mem: this.sysServ.getMem(),
-              disk: this.sysServ.getDisk(),
-              sys: this.sysServ.getSys()
-            })
-          )
-        )
-        .subscribe(data => {
-          this.cpu = data.cpu;
-          this.mem = data.mem;
-          this.disk = data.disk;
-          this.sys = data.sys;
-          this.lastUpdate = new Date();
-        });
-    //}
-  }
-
-  ngOnDestroy() {
-    if (this.refreshSub) this.refreshSub.unsubscribe();
+    forkJoin({
+      cpu: this.sysServ.getCPU(),
+      mem: this.sysServ.getMem(),
+      disk: this.sysServ.getDisk(),
+      sys: this.sysServ.getSys()
+    }).subscribe({
+      next: data => {
+        this.cpu = data.cpu;
+        this.mem = data.mem;
+        this.disk = data.disk;
+        this.sys = data.sys;
+        this.lastUpdate = new Date();
+      },
+      error: err => {
+        console.error('Failed to fetch system data:', err);
+      }
+    });
   }
 }
